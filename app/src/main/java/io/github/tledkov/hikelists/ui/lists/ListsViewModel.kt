f07a0d7341d4072c4926871d2bd901c430dda8a2
Taras ledkov <tledkov@apache.org>
2024-03-26 23:48:37 +0300

    wip

--- app/src/main/java/io/github/tledkov/hikelists/ui/lists/ListsViewModel.kt
+++ app/src/main/java/io/github/tledkov/hikelists/ui/lists/ListsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ListsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Please add some inventory to create backpack lists"
     }
     val text: LiveData<String> = _text
 }
