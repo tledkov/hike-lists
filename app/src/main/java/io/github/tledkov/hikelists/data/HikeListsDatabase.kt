f868a0851f15c47707d3fddf1c2655aaccf634ac
tledkov <tledkov@apache.org>
2026-02-24 18:19:57 +0300

    AI: сохранение списков

--- app/src/main/java/io/github/tledkov/hikelists/data/HikeListsDatabase.kt
+++ app/src/main/java/io/github/tledkov/hikelists/data/HikeListsDatabase.kt
@@ -46,7 +46,7 @@ abstract class HikeListsDatabase : RoomDatabase() {
             return Room
                 .databaseBuilder(context, HikeListsDatabase::class.java, DATABASE_NAME)
                 .addCallback(object : Callback() {
-                    override fun onOpen(db: SupportSQLiteDatabase) {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
                         super.onCreate(db)
                         // insert the data on the IO Thread
                         ioThread {
